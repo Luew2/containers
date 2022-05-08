b64d822c224f8f3c5d0c3b685ad4dabe53d1466e
Lucas Ewing <lucas.ewing2000@gmail.com>
2022-05-08 16:11:11 -0700

    Update fibonacci.py

--- containers/fibonacci.py
+++ containers/fibonacci.py
@@ -1,3 +1,56 @@
+################################################################################
+# example fibonacci number code;
+# you do not have to modify this code in any way
+################################################################################
+
+
+def fibs(n):
+    '''
+    This function computes the first n fibonacci numbers.
+    Notice that this function uses O(n) memory.
+    '''
+    fibs = []
+    fibs.append(1)
+    if n == 1:
+        return fibs
+    fibs.append(1)
+    while len(fibs) < n:
+        fibs.append(fibs[-1] + fibs[-2])
+    return fibs
+
+
+def fib_bad(n):
+    '''
+    This function computes the n-th fibonacci number,
+    but it uses O(n) memory to do so,
+    which is bad.
+    '''
+    return fibs(n)[-1]
+
+
+def fib(n):
+    '''
+    This function computes the n-th fibonacci number,
+    but it consumes only O(1) memory,
+    and is optimal.
+    '''
+    if n < 2:
+        return 1
+    f0 = 1
+    f1 = 1
+    for i in range(n - 1):
+        f2 = f1 + f0
+        f0 = f1
+        f1 = f2
+    return f2
+
+
+################################################################################
+# fibonacci number code using generators;
+# you will need to implement the functions below
+################################################################################
+
+
 class Fib:
     '''
     This class represents all the fibonacci numbers,
